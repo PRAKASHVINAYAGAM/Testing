6ce3fbbaf84bf94ef84e95f096e74ddfade42a64
prakash <prakash2vinayagam@gmail.com>
2024-12-11 11:55:25 +0530

    Initial commit for DifferentDatabase project

---
+++ src/test/java/com/demo/ManytoOne/DifferentDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.ManytoOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DifferentDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
